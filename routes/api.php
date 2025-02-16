362ed8d0bc12b15dd5bb6530489c13a6f2bbfa7e
Thai Tin Khang <kinyiasgit@gmail.com>
2025-02-16 10:46:26 +0700

    Update api category

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
