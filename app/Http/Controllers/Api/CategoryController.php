9065fb77fe334894eeb7823559f8c916957b5e3b
Thai Tin Khang <kinyiasgit@gmail.com>
2025-03-03 15:33:13 +0700

    Update brands api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -16,8 +18,9 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|unique:categories|string|max:255',
-            'description' => 'nullable|string',
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,',
+
         ]);
 
         $category = Category::create($request->all());
@@ -33,12 +36,19 @@ class CategoryController extends Controller
     public function update(Request $request, Category $category)
     {
         $request->validate([
-            'name' => 'required|string|max:255|unique:categories,name,' . $category->id,
-            'description' => 'nullable|string',
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,' . $category->id,
+       
         ]);
-
-        $category->update($request->all());
-
+        Log::info($category->name);
+        $category->name  = $request->name;
+        $category->slug = Str::slug($request->name);
+        // $image = $request->file('image');
+        // $file_extention = $request->file('image')->extension();
+        // $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+        // $this->GenerateCategoryThumnailsImage($image, $file_name);
+        // $category->image =  $file_name;
+        $category->save();
         return response()->json($category, 200);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BrandsController;
 use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -10,4 +11,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('brands', BrandsController::class);
 });
